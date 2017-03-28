b69179e6804e1297e019d8d0bbc1fd9b418aa334
Joseph Griffiths <dana@Josephs-MBP.home>
2017-03-28 15:21:42 +0100

    adding 3 for 2 on oranges

--- src/main/scala/CheckOut.scala
+++ src/main/scala/CheckOut.scala
@@ -1,25 +1,23 @@
+sealed trait Fruit { def price : BigDecimal }
 
-sealed trait Offer
-case object ThreeForTwo extends Offer
-case object BuyOneGetOneFree extends Offer
-
-
-sealed trait Fruit {
-  def price : BigDecimal
-  def offer: Offer }
-
-case class Apple(price: BigDecimal = 60, offer: Offer = BuyOneGetOneFree ) extends Fruit
-case class Orange(price: BigDecimal = 25, offer: Offer = ThreeForTwo ) extends Fruit
+case class Apple(price: BigDecimal = 60) extends Fruit
+case class Orange(price: BigDecimal = 25) extends Fruit
 
 object CheckOut {
 
   def totalPrice(shoppingCart: Seq[Fruit]): BigDecimal = {
     val priceOfApples = buyOneGetOneFree(shoppingCart)
-    val priceOfOranges = shoppingCart.collect { case _: Orange => Orange }.map(_.apply().price).sum
+    val priceOfOranges = threeForTwo(shoppingCart)
     priceOfApples + priceOfOranges
   }
 
-  private def buyOneGetOneFree(items: Seq[Fruit]): BigDecimal = {
+  def threeForTwo(items: Seq[Fruit]): BigDecimal = {
+    val oranges = items.collect { case orange: Orange => orange }
+    val orangesToChargeFor = (2 * (oranges.size/3) + (oranges.size %3))
+    orangesToChargeFor * Orange().price
+  }
+
+  private def buyOneGetOneFree[T](items: Seq[Fruit]): BigDecimal = {
     val apples = items.collect { case apple: Apple => apple }
     val applesToChargeFor = (apples.size / 2) + (apples.size % 2)
     applesToChargeFor * Apple().price
