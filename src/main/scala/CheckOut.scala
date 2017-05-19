1b4385cfbb954e6ee246889e37f9b58dccf18159
Joseph Griffiths <dana@Josephs-MBP.home>
2017-05-19 12:24:16 +0100

    whitespace

--- src/main/scala/CheckOut.scala
+++ src/main/scala/CheckOut.scala
@@ -22,7 +22,7 @@ abstract class Fruit {
 
 case object Apple extends Fruit {
   override val price: BigDecimal = .60
-  override val priceWithOffers: PriceFunction =Offers.buyOneGetOneFree
+  override val priceWithOffers: PriceFunction = Offers.buyOneGetOneFree
 }
 
 case object Orange extends Fruit {
@@ -34,6 +34,6 @@ object CheckOut {
   val TypesOfFruit: Seq[Fruit] = Seq(Apple, Orange)
   def countNumberOfItems(items: Seq[Fruit], item: Fruit): Int = items.count(_ == item)
 
-  def totalPrice(shoppingCart: Seq[Fruit]): BigDecimal =  TypesOfFruit.map(typeOfFruit =>
+  def totalPrice(shoppingCart: Seq[Fruit]): BigDecimal = TypesOfFruit.map(typeOfFruit =>
     typeOfFruit.priceWithOffers(countNumberOfItems(shoppingCart, typeOfFruit), typeOfFruit.price)).sum
 }
