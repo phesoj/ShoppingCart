ed420f9446197a0ac6335b4503b40759e1f566d7
Joseph Griffiths <dana@Josephs-MBP.home>
2017-05-19 12:23:15 +0100

    tidy up

--- src/main/scala/CheckOut.scala
+++ src/main/scala/CheckOut.scala
@@ -1,12 +1,7 @@
-sealed trait Fruit {
-  def price : BigDecimal
-  def priceWithOffers: (Int, BigDecimal) => BigDecimal
-}
-
-case class Apple(price: BigDecimal = .60, priceWithOffers: (Int, BigDecimal) => BigDecimal = Offers.buyOneGetOneFree) extends Fruit
-case class Orange(price: BigDecimal = .25,  priceWithOffers: (Int, BigDecimal) => BigDecimal = Offers.threeForTwo) extends Fruit
+import Offers.PriceFunction
 
 object Offers {
+  type PriceFunction = (Int, BigDecimal) => BigDecimal
   def buyOneGetOneFree(numberOfItems: Int, price: BigDecimal): BigDecimal = {
     val itemsToChargeFor = (numberOfItems / 2) + (numberOfItems % 2)
     itemsToChargeFor * price
@@ -16,12 +11,29 @@ object Offers {
     val itemsToChargeFor = 2 * (numberOfItems / 3) + (numberOfItems % 3)
     itemsToChargeFor * price
   }
+
+  def noOffers(numberOfItems: Int, price: BigDecimal): BigDecimal = numberOfItems * price
+}
+
+abstract class Fruit {
+  def price: BigDecimal
+  def priceWithOffers: PriceFunction
+}
+
+case object Apple extends Fruit {
+  override val price: BigDecimal = .60
+  override val priceWithOffers: PriceFunction =Offers.buyOneGetOneFree
+}
+
+case object Orange extends Fruit {
+  override val price: BigDecimal = .25
+  override val priceWithOffers: PriceFunction = Offers.threeForTwo
 }
 
 object CheckOut {
-  val TypesOfFruit: Seq[Fruit] = Seq(Apple(), Orange())
-  def countNumberOfItems(items: Seq[Fruit], name: Class[_ <: Fruit]) : Int = items.count(item => item.getClass.equals(name))
+  val TypesOfFruit: Seq[Fruit] = Seq(Apple, Orange)
+  def countNumberOfItems(items: Seq[Fruit], item: Fruit): Int = items.count(_ == item)
 
-  def totalPrice(shoppingCart: Seq[Fruit]) : BigDecimal =  TypesOfFruit.map ( typeOfFruit =>
-    typeOfFruit.priceWithOffers(countNumberOfItems(shoppingCart,typeOfFruit.getClass), typeOfFruit.price)).sum
+  def totalPrice(shoppingCart: Seq[Fruit]): BigDecimal =  TypesOfFruit.map(typeOfFruit =>
+    typeOfFruit.priceWithOffers(countNumberOfItems(shoppingCart, typeOfFruit), typeOfFruit.price)).sum
 }
